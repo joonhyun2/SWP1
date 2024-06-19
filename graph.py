07ff7a8b4f4ff35dd29c740938e6d7b2650ceea9
kmu-joon <joony52700@gmail.com>
2024-06-20 03:40:14 +0900

    modified image path

--- graph.py
+++ graph.py
@@ -1,16 +1,16 @@
-from cgi import parse_qs
+from urllib.parse import parse_qs
 from template import html
 import matplotlib.pyplot as plt
 
 def application(environ, start_response):
-    if environ['PATH_INFO'] == '/graph.png':
+    if environ['PATH_INFO'] == './사진/graph.png':
         try:
             with open('graph.png', 'rb') as f:
                 response_body = f.read()
         except:
             response_body = ''
         start_response('200 OK', [
-            ('Content-Type', 'image/png'),
+            ('Content-Type', ./사진/'image/png'),
             ('Content-Length', str(len(response_body)))
         ])
         return [response_body]
